b1730211ec7b3cfb0b4e82f30a7eaaaca90fa373
Kumar Sahil Sonal <ksskumarsahil006@gmail.com>
2026-09-03 15:44:42 +0530

    Time: 0 ms (100%), Space: 8.3 MB (99.26%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int sumeven=(n)*(4+(n-1)*2)/2;
-        int sumodd=(2*n)*((2*n)+1)/2;
-        int mi = min(sumeven,sumodd);
-        for(int i = mi;i>=1;i--){
-            if(sumodd%i==0 and sumeven%i==0)return i;
-        }
-        return 1;
+        // int sumeven=(n)*(4+(n-1)*2)/2;
+        // int sumodd=(2*n)*((2*n)+1)/2;
+        // int mi = min(sumeven,sumodd);
+        // for(int i = mi;i>=1;i--){
+        //     if(sumodd%i==0 and sumeven%i==0)return i;
+        // }
+        // return 1;
+        return n;
     }
 };
