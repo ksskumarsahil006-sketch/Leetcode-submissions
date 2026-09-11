6c7d94bd13513858709ebc7c3be143896dd4667a
Kumar Sahil Sonal <ksskumarsahil006@gmail.com>
2026-09-11 15:19:41 +0530

    Time: 7 ms (65.69%), Space: 30.8 MB (76.02%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     int totalNumbers(vector<int>& digits) {
-        int i  = 0,j=0,k=0;
+        int i = 0, j = 0, k = 0;
         int n = digits.size();
-        set<int>answers;
-        for(int i = 0;i < n;i++){
-            if(digits[i]==0)continue;
-            int l = digits[i]*100;
-            for(j = 0; j < n ;j++){
-                if(j==i)continue;
-                int m = digits[j]*10;
-                for(k=0;k< n;k++){
-                    if(k==i || k==j)continue;
-                    int current = l+m+digits[k];
-                    if(current%2==0)answers.insert(current);
-                } 
+        set<int> answers;
+        for (int i = 0; i < n; i++) {
+            if (digits[i] == 0)
+                continue;
+            int l = digits[i] * 100;
+            for (j = 0; j < n; j++) {
+                if (j == i)
+                    continue;
+                int m = digits[j] * 10;
+                for (k = 0; k < n; k++) {
+                    if (k == i || k == j)
+                        continue;
+                    int current = l + m + digits[k];
+                    if (current % 2 == 0)
+                        answers.insert(current);
+                }
             }
         }
         return answers.size();
