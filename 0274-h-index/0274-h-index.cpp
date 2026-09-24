179eaac5a074e16db20023de7d4a76d1582ed04b
Kumar Sahil Sonal <ksskumarsahil006@gmail.com>
2026-09-24 19:29:03 +0530

    Time: 1 ms (19.09%), Space: 13.8 MB (5.46%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        sort(citations.begin(),citations.end());
-        reverse(citations.begin(),citations.end());
+        sort(citations.rbegin(),citations.rend());
+        
         int ans= 0;
         for(int i  = 0;i<citations.size();i++){
             if(citations[i]>=i+1){
